fc78688f048286225b974e44e11955bb4fce37f7
colw <github.com/colw>
2015-02-24 19:55:37 +0100

    Readying for deployment

--- app.js
+++ app.js
@@ -55,10 +55,11 @@ var express = require('express');
 var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
 
-http.listen(3000, function() {
-	console.log('listening on *:3000');
+http.listen(app.get('port'), function() {
+  console.log("Node app is running at localhost:" + app.get('port'));
 });
 
 /* Socket */
