541c19362803d365e255b3d6fa68950f03480b29
colw <github.com/colw>
2015-03-04 10:31:09 +0100

    About section makeover
    - New words
    - Smalle font
    - Changed question mark to pointer.

--- dist/js/all.js
+++ dist/js/all.js
@@ -250,61 +250,46 @@ var HowCow = React.createClass({displayName: "HowCow",
 		return (
 			React.createElement("div", {id: "aboutCow"}, 
 
-                React.createElement("h2", {id: "cowwwwswhat"}, "What?"), 
-                    
-                React.createElement("p", null, "This feed won't give you instant gratification. At first it" + ' ' +
-                "won't give you anything. It's intended for a grazer. Come back" + ' ' +
-                "in a few minutes or maybe more, and you can see the news will" + ' ' +
-                "start to filter in."), 
-                
-                React.createElement("p", null, "Don't fret if you begin to see things you don't like." + ' ' +
-                "Vanish them. If you have a smaller appetite for humour, type", 
-                React.createElement("em", null, " '-theonion.com'"), " into the filter box (be mindful of" + ' ' +
-                "the dash out front). It's ok, no one will know."), 
-                
-                React.createElement("p", null, "Conversely, do you really enjoy stories about Australia? Or" + ' ' +
-                "Beef? ", React.createElement("em", null, "Enter"), " those words into the small box. Separately" + ' ' +
-                "if you please, but don't fret, I can remember more than one."), 
-                
-                React.createElement("p", null, "Click the tags to remove them. It's like they were never" + ' ' +
-                "there."), 
-
-                React.createElement("p", null, "Curious about where this news comes from? I'll show you." + ' ' +
-                "Click the word 'Sources'."), 
+        React.createElement("h2", {id: "cowwhat"}, "What?"), 
           
-                React.createElement("p", null, "Take your time, don't fret, the news won't stop. Even as you" + ' ' +
-                "read this."), 
-                
-                React.createElement("h2", {id: "cowwhy"}, "Why?"), 
-                
-                React.createElement("p", null, "I like reading news, and I like programming. You could also" + ' ' +
-                "say I like to reinvent the wheel, but take a closer look and" + ' ' +
-                "you may agree this feed is a little different."), 
-
-                React.createElement("p", null, "This site is intended for grazers and not those who find" + ' ' +
-                "themselves in a hurry. It intentionally shows nothing when you" + ' ' +
-                "load it, and from then it will only show news as it's" + ' ' +
-                "published. It's about the present, at a speed you can work" + ' ' +
-                "with."), 
-
-                React.createElement("p", null, "You can't set the source of the news, but you can curate" + ' ' +
-                "with broad, simple strokes. Searching is fast and efficient" + ' ' +
-                "too."), 
-
-                React.createElement("h2", {id: "cowhow"}, "How?"), 
-                
-                React.createElement("p", null, "Technically speaking, there is a ", React.createElement("a", {href: "http://www.nodejs.org", target: "_blank"}, "Node.js"), " backend on the look out for news. When it finds some, it will" + ' ' +
-                "send it straight to your browser using ", React.createElement("a", {
-                href: "http://socket.io", target: "_blank"}, "Socket.io"), ". The" + ' ' +
-                "whole rendering experience is based upon ", React.createElement("a", {
-                href: "http://facebook.github.io/react", 
-                target: "_blank"}, "React"), "."), 
+        React.createElement("p", null, "This feed won't give you instant gratification. At first it won't give you anything. It's intended" + ' ' +
+        "for grazing. Come back in a few minutes or maybe more, and you can see the news will start to filter" + ' ' +
+        "in."), 
+      
+        React.createElement("p", null, "Don't fret if you begin to see things you don't like. It's simple to filter."), 
         
-                React.createElement("p", null, "That's about enough tech speak for here. Read the ", React.createElement("a", {
-                href: "http://github.com/colw/nxws", target: "_blank"}, "code"), " if" + ' ' +
-                "you're curious.")
+        React.createElement("p", null, "If you have a smaller appetite for humour, type ", React.createElement("code", null, "'-theonion.com'"), " into the filter box (be" + ' ' +
+        "mindful of the dash out front). Conversely, do you really enjoy stories about Australia? Or Beef? Enter" + ' ' +
+        "those words into the filter box. I can remember more than one. Click the tags to remove them and it's" + ' ' +
+        "like they were never there."), 
 
-            )
+        React.createElement("p", null, "Curious about where this news comes from? Click the word 'Sources'."), 
+
+        React.createElement("p", null, "Take your time, the news won't stop. Even as you read this."), 
+      
+        React.createElement("h2", {id: "cowwhy"}, "Why?"), 
+      
+        React.createElement("p", null, "I like reading news, and I like programming. You could also say I like to reinvent the wheel, but" + ' ' +
+        "take a closer look and you may agree this feed is a little different."), 
+
+        React.createElement("p", null, "This site is intended for slow movers and not those who find themselves in a hurry. It intentionally" + ' ' +
+        "shows nothing when you load it, and from then it will only show news as it is published. It's about the" + ' ' +
+        "present, at a speed you can work with."), 
+
+        React.createElement("p", null, "You cannot set the source of the news, but you can curate with broad, simple strokes. Searching is" + ' ' +
+        "fast and efficient too."), 
+
+        React.createElement("h2", {id: "cowhow"}, "How?"), 
+      
+        React.createElement("p", null, "Technically speaking, there is a ", React.createElement("a", {href: "http://www.nodejs.org", target: "_blank"}, "Node.js"), " back end" + ' ' +
+        "on the look out for news. When it finds some, it will send it straight to your browser using ", React.createElement("a", {
+        href: "http://socket.io", target: "_blank"}, "Socket.io"), ". The whole rendering experience is based upon ", React.createElement("a", {
+        href: "http://facebook.github.io/react", target: "_blank"}, "React"), ". "), 
+      
+        React.createElement("p", null, React.createElement("a", {href: "http://gulpjs.com", target: "_blank"}, "Gulp"), " is a big help too, but that's about enough" + ' ' +
+        "tech. Read the ", React.createElement("a", {href: "http://github.com/colw/nxws", target: "_blank"}, "code"), " if you're curious.")
+
+      )
 		);
 	}
 });
--- src/jsx/news_view_about.jsx
+++ src/jsx/news_view_about.jsx
@@ -3,61 +3,46 @@ var HowCow = React.createClass({
 		return (
 			<div id="aboutCow">
 
-                <h2 id="cowwwwswhat">What?</h2>                
-                    
-                <p>This feed won't give you instant gratification. At first it
-                won't give you anything. It's intended for a grazer. Come back
-                in a few minutes or maybe more, and you can see the news will
-                start to filter in.</p>
-                
-                <p>Don't fret if you begin to see things you don't like.
-                Vanish them. If you have a smaller appetite for humour, type
-                <em> '-theonion.com'</em> into the filter box (be mindful of
-                the dash out front). It's ok, no one will know.</p>
-                
-                <p>Conversely, do you really enjoy stories about Australia? Or
-                Beef? <em>Enter</em> those words into the small box. Separately
-                if you please, but don't fret, I can remember more than one.</p>
-                
-                <p>Click the tags to remove them. It's like they were never
-                there.</p>
-
-                <p>Curious about where this news comes from? I'll show you.
-                Click the word 'Sources'.</p>
+        <h2 id="cowwhat">What?</h2>                
           
-                <p>Take your time, don't fret, the news won't stop. Even as you
-                read this.</p>
-                
-                <h2 id="cowwhy">Why?</h2>
-                
-                <p>I like reading news, and I like programming. You could also
-                say I like to reinvent the wheel, but take a closer look and
-                you may agree this feed is a little different.</p>
-
-                <p>This site is intended for grazers and not those who find
-                themselves in a hurry. It intentionally shows nothing when you
-                load it, and from then it will only show news as it's
-                published. It's about the present, at a speed you can work
-                with.</p>
-
-                <p>You can't set the source of the news, but you can curate
-                with broad, simple strokes. Searching is fast and efficient
-                too.</p>
-
-                <h2 id="cowhow">How?</h2>
-                
-                <p>Technically speaking, there is a <a href="http://www.nodejs.org" target="_blank">Node.js</a> backend on the look out for news. When it finds some, it will
-                send it straight to your browser using <a
-                href="http://socket.io" target="_blank">Socket.io</a>. The
-                whole rendering experience is based upon <a
-                href="http://facebook.github.io/react"
-                target="_blank">React</a>.</p>
+        <p>This feed won't give you instant gratification. At first it won't give you anything. It's intended
+        for grazing. Come back in a few minutes or maybe more, and you can see the news will start to filter
+        in.</p>
+      
+        <p>Don't fret if you begin to see things you don't like. It's simple to filter.</p>
         
-                <p>That's about enough tech speak for here. Read the <a
-                href="http://github.com/colw/nxws" target="_blank">code</a> if
-                you're curious.</p>
+        <p>If you have a smaller appetite for humour, type <code>'-theonion.com'</code> into the filter box (be
+        mindful of the dash out front). Conversely, do you really enjoy stories about Australia? Or Beef? Enter
+        those words into the filter box. I can remember more than one. Click the tags to remove them and it's
+        like they were never there.</p>
 
-            </div>
+        <p>Curious about where this news comes from? Click the word 'Sources'.</p>
+
+        <p>Take your time, the news won't stop. Even as you read this.</p>
+      
+        <h2 id="cowwhy">Why?</h2>
+      
+        <p>I like reading news, and I like programming. You could also say I like to reinvent the wheel, but
+        take a closer look and you may agree this feed is a little different.</p>
+
+        <p>This site is intended for slow movers and not those who find themselves in a hurry. It intentionally
+        shows nothing when you load it, and from then it will only show news as it is published. It's about the
+        present, at a speed you can work with.</p>
+
+        <p>You cannot set the source of the news, but you can curate with broad, simple strokes. Searching is
+        fast and efficient too.</p>
+
+        <h2 id="cowhow">How?</h2>
+      
+        <p>Technically speaking, there is a <a href="http://www.nodejs.org" target="_blank">Node.js</a> back end
+        on the look out for news. When it finds some, it will send it straight to your browser using <a
+        href="http://socket.io" target="_blank">Socket.io</a>. The whole rendering experience is based upon <a
+        href="http://facebook.github.io/react" target="_blank">React</a>. </p>
+      
+        <p><a href="http://gulpjs.com" target="_blank">Gulp</a> is a big help too, but that's about enough
+        tech. Read the <a href="http://github.com/colw/nxws" target="_blank">code</a> if you're curious.</p>
+
+      </div>
 		);
 	}
 });
